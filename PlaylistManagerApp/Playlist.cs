5187d483a73ec1dfb9046d23b5440afabef117f2
eushaahmed08 <eushaahmed08@gmail.com>
2025-08-04 14:02:39 +0600

    Added playlist class

---
+++ PlaylistManagerApp/Playlist.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistManagerApp
+{
+    internal class Playlist
+    {
+        public string Name { get; set; }
+        public List<Song> Songs { get; private set; }
+        public Playlist(string name)
+        {
+            Name = name;
+            Songs = new List<Song>();
+        }
+
+
+        //Method to add a song
+        public void AddSong(Song song)
+        {
+            if (song != null && !Songs.Any(s=>s.FilePath==song.FilePath))
+            {
+                Songs.Add(song);
+            }
+        }
+
+        //to remove a song
+        public void RemoveSong(Song song)
+        {
+            
+            Songs.Remove(song);
+            
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
